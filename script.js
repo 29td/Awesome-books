410ae6980b25c82f372a27235df813984c8aade4
Collins <mussocollins@gmail.com>
2022-07-05 13:48:49 +0300

    Make changes to script.js

--- script.js
+++ script.js
@@ -1,45 +1,83 @@
-const awesome = document.querySelector('.awesome-books')
+// Book Class
+class Book {
+    constructor(title, author) {
+      this.title = title;
+      this.author = author;
+    }
+  }
+  
+  // Storage Class: Handles local storage of books
+  class Storage {
+    
+    // Receives Books
+    static getBooks() {
+      let books;
+      if (localStorage.getItem('books') === null) {
+        books = [];
+      } else {
+        books = JSON.parse(localStorage.getItem('books'));
+      }
+      return books;
+    }
 
+    // Event: Adds Books
+  
+    static addBook(book) {
+      const books = Storage.getBooks();
+      books.push(book);
+      localStorage.setItem('books', JSON.stringify(books));
+    }
+  }
 
-const book = {
-    name: 'Lorem ipsum',
-    description: 'Testeroo Testyy'
-}
+  // UI Class: Displays listed Books
+  class UI {
+    static displayBooks() {
+      const books = Storage.getBooks();
+      books.forEach((book) => UI.addBookToList(book));
+    }
+  
+    static addBookToList(book) {
+      const list = document.querySelector('.book-collection');
+      const addedbook = document.createElement('div');
+      addedbook.innerHTML = `
+          <p>${book.title}</p>
+          <p>${book.author}</p>
+          <button type="submit" class="delete">Remove</button>
+          <hr>
+        `;
+      list.appendChild(addedbook);
+    }
 
-const book1 = {
-    name: 'Second book',
-    description: 'Testeroo Testyy'
-}
+    // Event: Deletes a Book
+    static deleteBook(el) {
+      if (el.classList.contains('delete')) {
+        el.parentElement.remove();
+      }
+    }
+  
+    // Event: Clears data
+    static clearFields() {
+      document.querySelector('#title').value = '';
+      document.querySelector('#author').value = '';
+    }
+  }
+  
+  document.addEventListener('DOMContentLoaded', UI.displayBooks);
+  document.querySelector('#form').addEventListener('submit', () => {
 
-const awesomeDiv = document.createElement('div');
-awesomeDiv.classList.add('awesome');
+    // Get form Values from the input fields
+    const title = document.querySelector('#title').value;
+    const author = document.querySelector('#author').value;
+    const book = new Book(title, author);
+    UI.addBookToList(book);
+    Storage.addBook(book);
+    UI.clearFields();
+  });
 
-awesome.innerHTML = `<h1>Awesome books</h1>
-<p>${book.name}</p>
-<p>${book.description}</p>
-<button type="submit" class="Remove">Remove</button>
-<hr>
-`;
-
-awesomeDiv.innerHTML = `<p>${book1.name}</p>
-<p>${book1.description}</p>
-<button type="submit" class="Remove">Remove</button>
-<hr>
-<input type="text" class="header" onclick="addButton() placeholder="Title"></input><br>
-<input type="text" class="author" onclick="addButton() placeholder="Author"></input><br><br>
-<p class="value"></p>
-<button type="submit" class="add" onclick="addButton()">Add</button>
-`;
-
-awesome.appendChild(awesomeDiv);
-
-
-
-const removeBtn = document.querySelector('.Remove');
-const addBtn = document.querySelector('.add');
-
-function addButton() {
-    document.querySelector('.value').innerHTML = 'ahdgn';
-    // awesome.style.color = 'blue';
-}
-
+// Event: Deletes a Book
+  document.querySelector('.book-collection').addEventListener('click', (e) => {
+    UI.deleteBook(e.target);
+    Storage.removeBook(e.target.parentElement.previousElementSibling.textContent);
+  });
+  
+  
